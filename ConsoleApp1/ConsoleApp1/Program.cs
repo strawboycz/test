ecc232fd7b592a483f3525bf7fbb8b3d216ff4ba
Miroslav Přeček <mirek.precek@seznam.cz>
2022-03-18 17:42:16 +0100

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,24 +1,26 @@
-﻿int soucet;
+﻿int soucet,pocet;
 string smrad = "";
+float prumer;
 Dictionary<string, string> smradosi = new Dictionary<string, string>();
 while (true)
 {
     soucet = 0;
     Console.WriteLine("\nZadejte jméno smraďocha:");
     string jmeno = Console.ReadLine();
-    for (int i = 0; i < jmeno.Length; i++)
+    for (pocet = 0; pocet < jmeno.Length; pocet++)
     {
-        soucet += (int) jmeno[i];
+        soucet += (int) jmeno[pocet];
     }
-    if (soucet % 7 == 0)
+    prumer = (float) soucet /pocet;
+    if ((int)prumer % 7 == 0)
     {
         smrad = "smrdí jako bolavá noha bezdomovce";
     }
-    else if (soucet % 5 == 0)
+    else if ((int)prumer % 5 == 0)
     {
         smrad = "smrdí jako koňská řiť";
     }
-    else if (soucet % 3 == 0)
+    else if ((int)prumer % 3 == 0)
     {
         smrad = "smrdí jako cibuláč";
     }
