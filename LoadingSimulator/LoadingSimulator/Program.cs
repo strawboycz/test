073a631413bd504dc6f9c3956f056e464a80cd35
Miroslav Přeček <mirek.precek@seznam.cz>
2022-04-09 19:46:11 +0200

    Update Program.cs

--- LoadingSimulator/LoadingSimulator/Program.cs
+++ LoadingSimulator/LoadingSimulator/Program.cs
@@ -61,8 +61,18 @@ namespace LoadingSimulator
 		private static void inctementBasicBar(int i, char barCharacter, int steps, int top, int timeOut)
 		{
 			Console.SetCursorPosition(i - 1, top);
-			Console.Write($"{barCharacter}  {i}/{steps} steps");
+			changeConsoleColor(i, steps);
+			Console.Write($"{barCharacter}");
+			Console.ResetColor();
+			Console.Write($"  {i}/{steps} steps");
 			Thread.Sleep(timeOut);
+		}
+
+		private static void changeConsoleColor(int i, int steps)
+		{
+			if (i <= Math.Round((double) (steps / 3))) Console.ForegroundColor = ConsoleColor.Red;
+			else if (i <= Math.Round((double)(steps / 3) * 2)) Console.ForegroundColor = ConsoleColor.DarkYellow;
+			else Console.ForegroundColor = ConsoleColor.Green;
 		}
 
 		private static int getRandomNumber(int low, int high)
@@ -73,11 +83,15 @@ namespace LoadingSimulator
 
 		private static void incrementAdvancedBar(int i, int steps, char level2Char, char level3Char)
 		{
+			
 			Console.SetCursorPosition(steps, 3);
+			Console.ResetColor();
 			Console.Write($"  {i + 1}/{steps} steps");
+			changeConsoleColor(i, steps);
 			Console.SetCursorPosition(i, 3);
 			Console.Write(level2Char);
 			Thread.Sleep(100);
+			changeConsoleColor(i, steps);
 			Console.SetCursorPosition(i, 3);
 			Console.Write(level3Char);
 			Thread.Sleep(100);
@@ -86,7 +100,11 @@ namespace LoadingSimulator
 		private static void prepareAdvancedBar(int steps, char level1Char, int top)
 		{
 			Console.SetCursorPosition(0, top);
-			for (int i = 1; i <= steps; i++) Console.Write(level1Char);
+			for (int i = 1; i <= steps; i++)
+			{
+				changeConsoleColor(i, steps);
+				Console.Write(level1Char);
+			}
 		}
 
 		#endregion
